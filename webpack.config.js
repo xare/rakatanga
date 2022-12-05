4c0286476f49bae37ad1256ac63d41592c9900dd
Juan Etxenike Almeida <xaresd@gmail.com>
2022-12-05 17:28:22 +0100

    updated the webpack encore bundle

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
 /*
@@ -75,6 +75,7 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+<<<<<<< ours
 .configureBabel((config) => {
     config.plugins.push('@babel/plugin-proposal-class-properties');
 })
@@ -90,6 +91,18 @@ Encore
 
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
+=======
+    // configure Babel
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
+
+    // enables and configure @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
+    })
+>>>>>>> theirs
 
 // uncomment if you use React
 //.enableReactPreset()
